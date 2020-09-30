9712c1d5be18ed7f8c97a47c76060b090a9f53e6
TimersHamlet <timershamlet@outlook.com>
2020-09-30 14:35:46 +0800

    单元测试

--- js-practice/practice.js
+++ js-practice/practice.js
@@ -1,6 +1,11 @@
 /**typeof js */
-var message ='something';
-console.log (typeof(message));
+var message = 'something';
+console.log(typeof message);
 console.log(typeof message);
 console.log(typeof 95);
-console.log('typeof null is',typeof null)
+console.log('typeof null is', typeof null);
+
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
---
+++ js-practice/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./practice');
+
+test('1+2=3',()=>{
+    expect(sum(1,2)).toBe(3);
+})
