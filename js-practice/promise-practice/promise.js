a0262e4514c1fccfb73fae75b8aa534bb5a2a59c
何佳乐(20017864) <20017864@suning.com>
2020-10-26 16:44:48 +0800

    promise理解练习

--- js-practice/promise-practice/promise.js
+++ js-practice/promise-practice/promise.js
@@ -12,22 +12,33 @@
 //     console.log (data)
 // })
 
-let p = new Promise((resolve, reject) => {
-  //做一些异步操作
-  setTimeout(function () {
-    var num = Math.ceil(Math.random() * 10); //生成1-10的随机数
-    if (num <= 5) {
-      resolve(num);
-    } else {
-      reject('数字太大了');
-    }
-  }, 2000);
+// let p = new Promise((resolve, reject) => {
+//   //做一些异步操作
+//   setTimeout(function () {
+//     var num = Math.ceil(Math.random() * 10); //生成1-10的随机数
+//     if (num <= 5) {
+//       resolve(num);
+//     } else {
+//       reject('数字太大了');
+//     }
+//   }, 2000);
+// });
+// p.then(
+//   data => {
+//     console.log('resolved', data);
+//   },
+//   err => {
+//     console.log('rejected', err);
+//   }
+// );
+
+let promise = new Promise(function(resolve, reject) {
+  console.log('Promise');
+  resolve();
 });
-p.then(
-  data => {
-    console.log('resolved', data);
-  },
-  err => {
-    console.log('rejected', err);
-  }
-);
+
+promise.then(function() {
+  console.log('resolved.');
+});
+
+console.log('Hi!');
